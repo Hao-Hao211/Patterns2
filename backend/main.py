6fd54076a4885f188c81782530363b0950540900
Hao-Hao211 <haozhang2004@gmail.com>
2025-08-14 23:56:48 +0800

    up

--- backend/main.py
+++ backend/main.py
@@ -1447,10 +1447,11 @@ db_pool = None
 # CORS配置
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://*.vercel.app"],
+    allow_origins=["http://localhost:3000", "https://*.vercel.app", "https://patterns2.vercel.app","https://haozhang.site"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 
